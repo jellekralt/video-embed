d8107a6f47b5f5772e693d1e2de67a606713f099
Jelle Kralt <jelle@jellekralt.nl>
2014-08-26 23:11:48 +0200

    Initial code commit

---
+++ index.js
@@ -0,0 +1,33 @@
+
+var template = function(type, url) {
+    switch(type) {
+        case 'youtube':
+            return '<iframe src="'+url+'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+        break;
+
+        case 'vimeo':
+            return '<iframe src="'+url+'" frameborder="0" allowfullscreen></iframe>';
+        break;
+    }
+}
+
+var embed = function(link) {
+    var pattern, match, position;
+    var patterns = [
+        {regex: /youtu\.be\/([\w\-.]+)/, type: 'youtube', url: '//www.youtube.com/embed/%1'},
+        {regex: /youtube\.com(.+)v=([^&]+)/, type: 'youtube', url: '//www.youtube.com/embed/%2'},
+        {regex: /vimeo\.com\/([0-9]+)/, type: 'vimeo', url: '//player.vimeo.com/video/%1'},
+        {regex: /vimeo\.com\/(.*)\/([0-9]+)/, type: 'vimeo', url: '//player.vimeo.com/video/%2'}
+    ];
+
+    for(var i=0;i<patterns.length;i++) {
+        pattern = patterns[i];
+
+        if(match = link.match(pattern.regex)) {
+            position = pattern.url.match(/%([0-9]+)/)[1];
+            return template(pattern.type, pattern.url.replace(/%([0-9]+)/, match[position]));
+        }
+    }
+}
+
+module.exports = exports = embed;
